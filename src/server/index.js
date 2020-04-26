48fa90217d8c01db2d549ad1ae7cfd35d8a0d10b
jeraquis <jeraquis@users.noreply.github.com>
2020-04-25 23:33:49 -0700

    setup
    basic structure, dependencies, loaders, and plugins

---
+++ src/server/index.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express()
+
+app.use(express.static('dist'))
+
+const cors = require('cors')
+app.use(cors())
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+//    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+app.listen(8001, function () {
+    console.log('Example app listening on port 8001!')
+})
